5b9726b87ef01f99183f512623b0eb23b234a214
Michał Więcek <wiecek1873@gmail.com>
2022-05-16 00:16:00 +0200

    Added ElevatorButton class to handle calling elevator

---
+++ Assets/Scripts/Elevator/ElevatorButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButton : Interactable
+{
+	[SerializeField] private Elevator _elevator;
+	[SerializeField] private ElevatorFloor _elevatorFloor;
+
+	public override void Use()
+	{
+		_elevator.Call(_elevatorFloor);
+	}
+
+	public override void OnFocus()
+	{
+		base.OnFocus();
+
+		Debug.Log("Focus");
+	}
+
+	public override void OnFocusExit()
+	{
+		base.OnFocusExit();
+
+		Debug.Log("FocusExit");
+	}
+}
