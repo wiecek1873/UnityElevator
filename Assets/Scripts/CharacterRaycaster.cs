dd46070b91801d222f608e14a98722c9b5136354
Michał Więcek <wiecek1873@gmail.com>
2022-05-15 23:14:24 +0200

    Added interactable layer and character raycaster to interact with enviroment

---
+++ Assets/Scripts/CharacterRaycaster.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRaycaster : MonoBehaviour
+{
+	public Interactable CurrentFocus { get; private set; }
+
+	[SerializeField] private Transform _origin;
+	[SerializeField] private LayerMask _targetLayers;
+	[SerializeField] private float _maxDistance = 5f;
+
+	private void OnRaycastHit(RaycastHit raycastHit)
+	{
+		if (CurrentFocus != null && CurrentFocus.gameObject == raycastHit.collider.gameObject)
+			return;
+
+		if (CurrentFocus != null)
+			CurrentFocus.OnFocusExit();
+
+		CurrentFocus = raycastHit.collider.GetComponent<Interactable>();
+		CurrentFocus.OnFocus();
+	}
+
+	private void OnRaycastMiss()
+	{
+		if (CurrentFocus != null)
+			CurrentFocus.OnFocusExit();
+
+		CurrentFocus = null;
+	}
+
+	private void Update()
+	{
+		Ray ray = new Ray(_origin.position, _origin.forward);
+
+		if (Physics.Raycast(ray, out RaycastHit raycastHit, _maxDistance, _targetLayers))
+			OnRaycastHit(raycastHit);
+		else
+			OnRaycastMiss();
+	}
+}
