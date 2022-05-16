6aab76e7cb0a8fdd000cf363fb2de351c90c7bdd
Michał Więcek <wiecek1873@gmail.com>
2022-05-16 15:24:38 +0200

    More accurate name

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-	[SerializeField] private bool _hideCursor;
+	[SerializeField] private bool _hideCursorOnStart;
 
 	private void Start()
 	{
-		if (_hideCursor)
+		if (_hideCursorOnStart)
 		{
 			Cursor.lockState = CursorLockMode.Locked;
 			Cursor.visible = false;
