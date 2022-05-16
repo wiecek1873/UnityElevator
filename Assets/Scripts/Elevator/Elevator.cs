d0721dc8e2693f765c4a0c85ee395b5d206960b4
Michał Więcek <wiecek1873@gmail.com>
2022-05-16 12:28:54 +0200

    Added public boolean property in ElevatorDoorsAnimator to check if doors are closed

--- Assets/Scripts/Elevator/Elevator.cs
+++ Assets/Scripts/Elevator/Elevator.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
@@ -8,6 +9,8 @@ public class Elevator : MonoBehaviour
 	public bool IsMoving { get; private set; }
 	public bool IsMovingUp { get; private set; }
 
+	[SerializeField] private Rigidbody _rigidbody;
+	[SerializeField] private ElevatorDoorsAnimator _elevatorDoorsAnimator;
 	[SerializeField] private float _speed = 2f;
 
 	private List<ElevatorFloor> _floorsToVisit;
@@ -22,13 +25,18 @@ public class Elevator : MonoBehaviour
 		_floorsToVisit.Add(elevatorFloor);
 	}
 
-	private void MoveToNextFloor()
+	private IEnumerator MoveToNextFloor()
 	{
 		ElevatorFloor floorToVisit = _floorsToVisit.First();
 		_floorsToVisit.RemoveAt(0);
 
+		_elevatorDoorsAnimator.CloseDoors();
+
+		while (!_elevatorDoorsAnimator.DoorsAreClosed)
+			yield return null;
+
 		float moveDuration = Vector3.Distance(transform.position, floorToVisit.ElevatorPosition) / _speed;
-		transform.DOMove(floorToVisit.ElevatorPosition, moveDuration)
+		_rigidbody.DOMove(floorToVisit.ElevatorPosition, moveDuration)
 			.SetEase(Ease.InOutQuart)
 			.SetUpdate(UpdateType.Fixed);
 	}
@@ -49,7 +57,7 @@ public class Elevator : MonoBehaviour
 	{
 		if (!IsMoving && _floorsToVisit.Count > 0)
 		{
-			MoveToNextFloor();
+			StartCoroutine(MoveToNextFloor());
 			IsMoving = true;
 		}
 	}
--- Assets/Scripts/Elevator/ElevatorDoorsAnimator.cs
+++ Assets/Scripts/Elevator/ElevatorDoorsAnimator.cs
@@ -3,12 +3,38 @@ using DG.Tweening;
 
 public class ElevatorDoorsAnimator : MonoBehaviour
 {
+	public bool DoorsAreClosed { get; private set; }
+
 	[SerializeField] private ElevatorDoorsTriggerHandler _elevatorDoorsTriggerHandler;
 	[SerializeField] private Animator _animator;
 	[SerializeField] private string _animatorParameterName;
+	[SerializeField] private float _closeTime = 2.5f;
 
 	private float _doorsOpenness = 1;
 	private Tween _activeTween;
+
+	public void OpenDoors()
+	{
+		DoorsAreClosed = false;
+
+		_activeTween.Kill();
+
+		_doorsOpenness = _animator.GetFloat(_animatorParameterName);
+
+		_activeTween = DOTween.To(() => _doorsOpenness, x => _doorsOpenness = x, 1, _closeTime - _doorsOpenness)
+			.OnUpdate(() => _animator.SetFloat(_animatorParameterName, _doorsOpenness));
+	}
+
+	public void CloseDoors()
+	{
+		_activeTween.Kill();
+
+		_doorsOpenness = _animator.GetFloat(_animatorParameterName);
+
+		_activeTween = DOTween.To(() => _doorsOpenness, x => _doorsOpenness = x, 0, _doorsOpenness * _closeTime)
+			.OnUpdate(() => _animator.SetFloat(_animatorParameterName, _doorsOpenness))
+			.OnComplete(() => DoorsAreClosed = true);
+	}
 
 	protected virtual void OnCharacterBetweenDoorsEntered()
 	{
@@ -18,26 +44,6 @@ public class ElevatorDoorsAnimator : MonoBehaviour
 	protected virtual void OnCharacterBetweenDoorsExited()
 	{
 		//CloseDoors();
-	}
-
-	private void OpenDoors()
-	{
-		_activeTween.Kill();
-
-		_doorsOpenness = _animator.GetFloat(_animatorParameterName);
-
-		_activeTween = DOTween.To(() => _doorsOpenness, x => _doorsOpenness = x, 1, 1 - _doorsOpenness)
-			.OnUpdate(() => _animator.SetFloat(_animatorParameterName, _doorsOpenness));
-	}
-
-	private void CloseDoors()
-	{
-		_activeTween.Kill();
-
-		_doorsOpenness = _animator.GetFloat(_animatorParameterName);
-
-		_activeTween = DOTween.To(() => _doorsOpenness, x => _doorsOpenness = x, 0, _doorsOpenness)
-			.OnUpdate(() => _animator.SetFloat(_animatorParameterName, _doorsOpenness));
 	}
 
 	private void Start()
