ecf586bce713d01980e1ecc2713dfdca0da47ff7
Michał Więcek <wiecek1873@gmail.com>
2022-05-17 12:31:41 +0200

    Added MoveWithPlatform method to CharacterMovement for better platfor moving experience

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -5,20 +5,31 @@ public class CharacterMovement : MonoBehaviour
 	[SerializeField] private CharacterController _characterController;
 	[SerializeField] private float _playerSpeed = 2.0f;
 	[SerializeField] private float _jumpHeight = 1.0f;
-	[SerializeField] private float _gravity = -50f;
-	[SerializeField] private float _slopeRaycastDistance = 1f;
+	[SerializeField] private float _groundRaycastDistance = 0.5f;
+	[SerializeField] private float _slopeRaycastDistance = 0.35f;
 	[SerializeField] private float _slopeForce = 10f;
+	[SerializeField] private float _platformRaycastDistance = 0.5f;
 
 	private Vector3 _playerVelocity;
+	private GameObject _platformLastFrame;
+	private Vector3 _platformPositionLastFrame;
 
-	private bool IsGrounded()
+	private bool OnGround()
 	{
-		return _characterController.isGrounded;
+		if(_characterController.isGrounded)
+			return true;
+
+		Vector3 characterBase = CharacterBase();
+		float raycastDistance = _groundRaycastDistance + _characterController.skinWidth;
+
+		Ray ray = new Ray(characterBase, Vector3.down * raycastDistance);
+
+		return Physics.Raycast(ray, out RaycastHit raycastHit, raycastDistance);
 	}
 
 	private bool OnSlope()
 	{
-		Vector3 characterBase = _characterController.transform.position + Vector3.down * _characterController.height / 2f;
+		Vector3 characterBase = CharacterBase();
 		float raycastDistance = _slopeRaycastDistance + _characterController.skinWidth;
 
 		Ray ray = new Ray(characterBase, Vector3.down * raycastDistance);
@@ -49,22 +60,61 @@ public class CharacterMovement : MonoBehaviour
 		_playerVelocity.y += Physics.gravity.y * Time.deltaTime;
 	}
 
+	private Vector3 CharacterBase()
+	{
+		return _characterController.transform.position + Vector3.down * _characterController.height / 2f;
+	}
+
+	private void MoveWithPlatform()
+	{
+		Vector3 characterBase = CharacterBase();
+		float raycastDistance = _platformRaycastDistance + _characterController.skinWidth;
+
+		Ray ray = new Ray(characterBase, Vector3.down * raycastDistance);
+
+		if(Physics.Raycast(ray, out RaycastHit raycastHit, raycastDistance))
+		{
+			//todo Refactor this into readable
+			if(_platformLastFrame != null)
+			{
+				if(_platformLastFrame == raycastHit.collider.gameObject)
+				{
+					_characterController.Move(_platformLastFrame.transform.position - _platformPositionLastFrame);
+					_platformPositionLastFrame = _platformLastFrame.transform.position;
+				}
+				else
+				{
+					_platformLastFrame = raycastHit.collider.gameObject;
+					_platformPositionLastFrame = _platformLastFrame.transform.position;
+				}
+			}
+			else
+			{
+				_platformLastFrame = raycastHit.collider.gameObject;
+				_platformPositionLastFrame = _platformLastFrame.transform.position;
+			}
+		}
+	}
+
 	private void Update()
 	{
-		bool characterGrounded = IsGrounded();
+		bool onGround = OnGround();
 
-		if (characterGrounded && _playerVelocity.y < 0)
+		Debug.Log(onGround);
+		
+		if (onGround && _playerVelocity.y < 0)
 			ResetVelocityOnGround();
 
 		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
 		MoveHorizontally(moveDirection);
 
-		if (Input.GetButtonDown("Jump") && characterGrounded)
+		if (Input.GetButtonDown("Jump") && onGround)
 			Jump();
 
 		if (OnSlope() && _playerVelocity.y >= 0)
 			_characterController.Move(Vector3.down * Time.deltaTime * _slopeForce);
+
+		MoveWithPlatform();
 
 		HandleGravity();
 
